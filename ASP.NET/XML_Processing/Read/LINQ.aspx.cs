f5d7f1cf3aee85bdbb1f1a26eeead7928494686c
superyaooo <superyaooo@gmail.com>
2016-09-21 17:37:47 -0500

    added nested linq example

--- ASP.NET/XML_Processing/Read/LINQ.aspx.cs
+++ ASP.NET/XML_Processing/Read/LINQ.aspx.cs
@@ -19,7 +19,7 @@ private void SelectLINQSingle()
     var xElem = XElement.Load("App_Data/employees.xml");
 
     // get a specific employee
-    var emp = (from e in xElem.Descendants("Employee")) 
+    var emp = (from e in xElem.Descendants("Employee")
                 where e.Element("id").Value == "1"
                 select e).SingleOrDefault();
 
@@ -57,4 +57,13 @@ private void SelectLINQMultiple()
     {
         sb.AppendFormat("id={0}", emp.Element("id").Value);
     }
+}
+
+private void SelectNestedLINQ()
+{
+    // select "artist" inside "attendee" with sub nodes "entry" whose @approveStatus is "1"
+    var artists = from artist in xDoc.Root.Descendants("attendee")
+                from entry in artist.Elements("entry")
+                where entry.Attribute("approveStatus").Value == "1"
+                select artist;
 }
