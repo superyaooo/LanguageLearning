bc0ec24bff299cb16474a2096212f723f0da8c00
superyaooo <superyaooo@gmail.com>
2016-11-01 21:25:45 -0500

    added comment in fizzbuzz

--- C#/Katas/FizzBuzz.cs
+++ C#/Katas/FizzBuzz.cs
@@ -1,7 +1,39 @@
+/*
+* Write a program that outputs the string representation of numbers from 1 to n.
+*
+* But for multiples of three it should output “Fizz” instead of the number and * for the multiples of five output “Buzz”. For numbers which are multiples of
+* both three and five output “FizzBuzz”.
+*
+* Example:
+* n = 15,
+*
+* Return:
+* [
+*    "1",
+*    "2",
+*    "Fizz",
+*    "4",
+*    "Buzz",
+*    "Fizz",
+*    "7",
+*    "8",
+*    "Fizz",
+*    "Buzz",
+*    "11",
+*    "Fizz",
+*    "13",
+*    "14",
+*    "FizzBuzz"
+* ]
+*
+*/
+
+
+
 public class Solution {
     public IList<string> FizzBuzz(int n) {
         List<string> fbList = new List<string>();
-        
+
         for(var i=1; i<=n; i++){
             if(i%15==0) {
                 fbList.Add("FizzBuzz");
@@ -16,7 +48,7 @@ public class Solution {
                 fbList.Add(i.ToString());
             }
         }
-        
+
         return fbList;
     }
 }
