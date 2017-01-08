4322d39c0eb605fb7a726f25376efa491fe14bc5
superyaooo <superyaooo@gmail.com>
2017-01-08 12:45:53 -0600

    restructered java folder;updated CalcEngine demo with classes and methods

--- Java/FirstSample.java
+++
@@ -1,7 +0,0 @@
-public class FirstSample
-{
-    public static void main(String[] args)
-    {
-        System.out.println("Test testing 1 2 3...");
-    }
-}
--- Java/organized/src/com/pluralsight/getorganized/Main.java
+++
@@ -1,8 +0,0 @@
-package com.pluralsight.getorganized;
-
-public class Main {
-
-    public static void main(String[] args) {
-	    System.out.println("Hello Get Organized");
-    }
-}
