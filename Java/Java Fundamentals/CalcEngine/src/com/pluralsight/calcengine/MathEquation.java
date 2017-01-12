2576c7d3aed4408bb59e65d119441e885de00377
superyaooo <superyaooo@gmail.com>
2017-01-11 21:28:11 -0600

    updated CalcEngine with Field Initializers and Constructors

--- Java/Java Fundamentals/CalcEngine/src/com/pluralsight/calcengine/MathEquation.java
+++ Java/Java Fundamentals/CalcEngine/src/com/pluralsight/calcengine/MathEquation.java
@@ -6,7 +6,7 @@ package com.pluralsight.calcengine;
 public class MathEquation {
     private double leftVal;
     private double rightVal;
-    private char opCode;
+    private char opCode = 'a';
     private double result;
 
     public double getLeftVal() {return leftVal;}
@@ -17,6 +17,20 @@ public class MathEquation {
     public void setOpCode(char opCode) {this.opCode = opCode;}
 
     public double getResult() {return result;}
+
+    public MathEquation() {
+
+    }
+
+    public MathEquation(char opCode) {
+        this.opCode = opCode;
+    }
+
+    public MathEquation(char opCode, double leftVal, double rightVal) {
+        this(opCode);
+        this.leftVal = leftVal;
+        this.rightVal = rightVal;
+    }
 
     public void execute() {
         switch (opCode) {
