36a5a7718c2d88cf75a848c0adcc3fdeb4dee677
superyaooo <superyaooo@gmail.com>
2017-01-22 17:03:11 -0600

    Updated CalcEngine with Exceptions and Error Handling

---
+++ Java/Java Fundamentals/CalcEngine/src/com/pluralsight/calcengine/InvalidStatementException.java
@@ -0,0 +1,14 @@
+package com.pluralsight.calcengine;
+
+/**
+ * Created by yaowang on 1/22/17.
+ */
+public class InvalidStatementException extends Exception {
+    public InvalidStatementException(String reason, String statement) {
+        super(reason + ": " + statement);
+    }
+
+    public InvalidStatementException(String reason, String statement, Throwable cause) {
+        super(reason + ": " + statement, cause);
+    }
+}
--- Java/Java Fundamentals/CalcEngine/src/com/pluralsight/calcengine/Main.java
+++ Java/Java Fundamentals/CalcEngine/src/com/pluralsight/calcengine/Main.java
@@ -7,6 +7,9 @@ public class Main {
 //        useCalculatorBase();
 
         String[] statements = {
+                "add 1.0",
+                "add xx 25.0",
+                "addX 0.0 0.0",
                 "divide 100.0 50.0",
                 "add 25.0 92.0",
                 "subtract 225.0 17.0",
@@ -15,8 +18,15 @@ public class Main {
 
         CalculateHelper helper = new CalculateHelper();
         for(String statement:statements) {
-            helper.process(statement);
-            System.out.println(helper);
+            try {
+                helper.process(statement);
+                System.out.println(helper);
+            } catch (InvalidStatementException e) {
+                System.out.println(e.getMessage());
+                if(e.getCause() != null)
+                    System.out.println(" Original exception: " + e.getCause().getMessage());
+            }
+
         }
     }
 
