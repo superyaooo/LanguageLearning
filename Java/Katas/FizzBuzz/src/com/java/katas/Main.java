fa31d6e45313c3581fe3724526bb6db46b70fdb2
superyaooo <superyaooo@gmail.com>
2017-01-26 21:27:27 -0600

    added reversed string kata

--- Java/Katas/FizzBuzz/src/com/java/katas/Main.java
+++ Java/Katas/FizzBuzz/src/com/java/katas/Main.java
@@ -3,6 +3,32 @@ package com.java.katas;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Write a program that outputs the string representation of numbers from 1 to n.
+ * But for multiples of three it should output “Fizz” instead of the number and for the multiples of five output “Buzz”. For numbers which are multiples of both three and five output “FizzBuzz”.
+ *
+ * Example:
+ * n = 15,
+ * Return:
+ * [
+ *   "1",
+ *   "2",
+ *   "Fizz",
+ *   "4",
+ *   "Buzz",
+ *   "Fizz",
+ *   "7",
+ *   "8",
+ *   "Fizz",
+ *   "Buzz",
+ *   "11",
+ *   "Fizz",
+ *   "13",
+ *   "14",
+ *   "FizzBuzz"
+ * ]
+ */
+
 public class Main {
 
     public static void main(String[] args) {
--- Java/Katas/Reverse String/src/com/java/katas/Main.java
+++ Java/Katas/Reverse String/src/com/java/katas/Main.java
@@ -1,16 +1,39 @@
 package com.java.katas;
 
-
-import java.util.Arrays;
-import java.util.Collections;
+/**
+ * Write a function that takes a string as input and returns the string reversed.
+ *
+ * Example:
+ * Given s = "hello", return "olleh".
+ */
 
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println(reverseString("hello"));
+        System.out.println(reverseStringSB("hello"));
+        System.out.println(reverseStringSwap("hello"));
     }
 
-    public static String reverseString(String s) {
+    // reverse using String Builder reverse function
+    public static String reverseStringSB(String s) {
+        StringBuilder sb = new StringBuilder();
+        sb.append(s);
+        return sb.reverse().toString();
+    }
 
+    // swap start and end chars
+    public static String reverseStringSwap(String s) {
+        char[] newS = s.toCharArray();
+        int begin = 0;
+        int end = newS.length-1;
+        while(begin <= end) {
+            char temp = newS[begin];
+            newS[begin] = newS[end];
+            newS[end] = temp;
+            begin++;
+            end--;
+        }
+
+        return new String(newS);
     }
 }
