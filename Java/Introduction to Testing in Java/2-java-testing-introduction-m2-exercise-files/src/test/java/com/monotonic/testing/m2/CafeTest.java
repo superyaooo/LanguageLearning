ce826ec2573d48153d893d189287b37b923a3eb9
superyaooo <superyaooo@gmail.com>
2017-02-17 16:28:59 -0600

    added Introduction to Testing in Java

---
+++ Java/Introduction to Testing in Java/2-java-testing-introduction-m2-exercise-files/src/test/java/com/monotonic/testing/m2/CafeTest.java
@@ -0,0 +1,20 @@
+package com.monotonic.testing.m2;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class CafeTest
+{
+    @Test
+    public void canBrewEspresso()
+    {
+        Cafe cafe = new Cafe();
+        cafe.restockBeans(7);
+
+        Coffee coffee = cafe.brew(CoffeeType.Espresso);
+
+        Assert.assertEquals(CoffeeType.Espresso, coffee.getType());
+        Assert.assertEquals(0, coffee.getMilk());
+        Assert.assertEquals(7, coffee.getBeans());
+    }
+}
