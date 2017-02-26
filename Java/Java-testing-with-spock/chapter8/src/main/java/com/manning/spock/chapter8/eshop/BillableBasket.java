67473a81cf2689eaaf647476da61c91cf5c93d6f
superyaooo <superyaooo@gmail.com>
2017-02-26 13:39:17 -0600

    add Java Testing with Spock directory

---
+++ Java/Java-testing-with-spock/chapter8/src/main/java/com/manning/spock/chapter8/eshop/BillableBasket.java
@@ -0,0 +1,30 @@
+package com.manning.spock.chapter8.eshop;
+
+import java.util.Map.Entry;
+
+public class BillableBasket extends Basket{
+
+	private CreditCardProcessor creditCardProcessor;
+
+	public void setCreditCardProcessor(CreditCardProcessor creditCardProcessor) {
+		this.creditCardProcessor = creditCardProcessor;
+	}
+	
+	public boolean checkout(Customer customer)	{
+		CreditCardResult result = creditCardProcessor.sale(findOrderPrice(), customer);
+		creditCardProcessor.shutdown();
+		
+		return result == CreditCardResult.OK;
+	}
+	
+	private int findOrderPrice() {
+		int sum = 0;
+		for (Entry<Product, Integer> entry : contents.entrySet()) {
+			int count = entry.getValue();
+			int price = entry.getKey().getPrice();
+			sum = sum + (count * price);
+		}
+		return sum;
+	}
+	
+}
