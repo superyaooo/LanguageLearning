6e070f10ba4a5af8ae21998bd4e47d0f2c3e3514
superyaooo <superyaooo@gmail.com>
2017-08-22 09:53:27 -0500

    add more java lambda examples

--- Java/Snippets/LambdaExpression.java
+++ Java/Snippets/LambdaExpression.java
@@ -14,3 +14,18 @@ list.forEach(m -> System.out.println(m.getLabel()));	// result: v1 v2 v3
 // removeIf & lambda expression
 list.removeIf(m -> m.getValue().equals("abc"));		// remove v1 and v3 based on value
 
+
+// List.sort() with lambda, int
+list.sort((Person m, Person n) -> m.getAge() - n.getAge());
+// parameter type is optional for lambda
+list.sort((m, n) -> m.getAge() - n.getAge());
+
+// List.sort() with lambda, String
+list.sort((m, n) -> m.getName().compareTo(n.getName()));
+
+// List.sort() with lambda, BigDecimal
+list.sort((m, n) -> m.getSalary().compareTo(n.getSalary()));
+
+// reversed sorting
+Comparator<Person> salaryComparator = (m, n) -> m.getSalary().compareTo(n.getSalary());
+list.sort(salaryComparator.reversed());
